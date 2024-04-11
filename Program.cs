4603653ffa2b74f21aa062bba54d96785af56c45
Anouar EL KHALFI <147706023+lakhlifia@users.noreply.github.com>
2024-04-11 12:02:41 +0200

    test

--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-Console.WriteLine("helloword");
-hello word 
+//ceci est un commentaire 
+Console.WriteLine("j'apprends le c#");
+// le Write et Write Line différence 
+Console.Write("exemple ");
+// declaration de variable: Type nom = valeur;
+// les types :int,string , bool /// nom 
+Console.Write("exemple ");
