7bf04c73b0dd06c7c96cc60e84acd5985c8b9f9e
anouar elkhalfi <anouarelkhalfi@gmail.com>
2024-04-15 09:45:27 +0200

    test

--- Program.cs
+++ Program.cs
@@ -1,19 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-//ceci est un commentaire 
+//ceci est un commentaire
 Console.WriteLine("j'apprends le c#");
-// le Write et Write Line différence 
+// le Write et Write Line différence
 Console.Write("exemple ");
 // declaration de variable: Type nom = valeur;
 // les types :int,string , bool /// nom : estVivan
 int age = 23;
 string prenom = "anouar";
 bool estVivan = true;
-// typage implicite 
+// typage implicite
 var lastName = "el khalfi";
 var taille = 128;
 var faux = false;
 // reaffecter une variable
 taille = 200;
-
-    
+// test
