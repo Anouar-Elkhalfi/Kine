121070e286236a570ee02f1ce7db57fa24c9ffb0
anouar elkhalfi <anouarelkhalfi@gmail.com>
2024-04-15 09:43:39 +0200

    S3

--- Program.cs
+++ Program.cs
@@ -16,43 +16,4 @@ var faux = false;
 // reaffecter une variable
 taille = 200;
 
-using System;
-
-class Program
-{
-    static void Main(string[] args)
-    {
-        // Récupération du premier nombre saisi par l'utilisateur
-        Console.WriteLine("Entrez le premier nombre:");
-        string input1 = Console.ReadLine();
-        int number1;
-        while (!int.TryParse(input1, out number1))
-        {
-            Console.WriteLine("Veuillez entrer un nombre valide:");
-            input1 = Console.ReadLine();
-        }
-
-        // Récupération du deuxième nombre saisi par l'utilisateur
-        Console.WriteLine("Entrez le deuxième nombre:");
-        string input2 = Console.ReadLine();
-        int number2;
-        while (!int.TryParse(input2, out number2))
-        {
-            Console.WriteLine("Veuillez entrer un nombre valide:");
-            input2 = Console.ReadLine();
-        }
-
-        // Demande du message de succès
-        Console.WriteLine("Entrez un message de succès:");
-        string successMessage = Console.ReadLine();
-
-        // Addition des deux nombres
-        int result = number1 + number2;
-
-        // Affichage de l'opération et du résultat
-        Console.WriteLine($"{number1} + {number2} = {result}");
-
-        // Affichage du message de succès
-        Console.WriteLine(successMessage);
-    }
-}
+    
