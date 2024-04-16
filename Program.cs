a1d025464f0e1f7657fba75cc810610823878f0b
Anouar EL KHALFI <147706023+Anouar-Elkhalfi@users.noreply.github.com>
2024-04-16 13:56:43 +0200

    les classes

--- Program.cs
+++ Program.cs
@@ -1,18 +1,75 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-//ceci est un commentaire
-Console.WriteLine("j'apprends le c#");
-// le Write et Write Line différence
-Console.Write("exemple ");
-// declaration de variable: Type nom = valeur;
-// les types :int,string , bool /// nom : estVivan
-int age = 23;
-string prenom = "anouar";
-bool estVivan = true;
-// typage implicite
-var lastName = "el khalfi";
-var taille = 128;
-var faux = false;
-// reaffecter une variable
-taille = 200;
-// test
+// les classes
+using System;
+
+class Character
+{
+    public string Name { get; set; }
+    public int HealthPoints { get; set; }
+    public int AttackStrength { get; set; }
+    public int DefenseStrength { get; set; }
+
+    public Character(string name, int healthPoints, int attackStrength, int defenseStrength)
+    {
+        Name = name;
+        HealthPoints = healthPoints;
+        AttackStrength = attackStrength;
+        DefenseStrength = defenseStrength;
+    }
+
+    public bool IsAlive()
+    {
+        return HealthPoints > 0;
+    }
+
+    public void Attack(Character target)
+    {
+        if (!IsAlive())
+        {
+            Console.WriteLine($"{Name} est déjà mort et ne peut pas attaquer.");
+            return;
+        }
+
+        if (!target.IsAlive())
+        {
+            Console.WriteLine($"{target.Name} est déjà mort et ne peut pas être attaqué.");
+            return;
+        }
+
+        int damage = Math.Max(AttackStrength - target.DefenseStrength, 0);
+        target.HealthPoints -= damage;
+
+        Console.WriteLine($"{Name} attaque {target.Name} et lui inflige {damage} points de dégâts.");
+
+        if (!target.IsAlive())
+        {
+            Console.WriteLine($"{target.Name} est mort !");
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Character player1 = new Character("Joueur 1", 100, 20, 10);
+        Character player2 = new Character("Joueur 2", 100, 15, 15);
+
+        Console.WriteLine("Début du combat !");
+
+        while (player1.IsAlive() && player2.IsAlive())
+        {
+            player1.Attack(player2);
+            if (!player2.IsAlive()) break; // Vérifie si le joueur 2 est encore en vie après chaque attaque de joueur 1
+            player2.Attack(player1);
+        }
+
+        if (player1.IsAlive())
+        {
+            Console.WriteLine($"{player1.Name} a gagné !");
+        }
+        else
+        {
+            Console.WriteLine($"{player2.Name} a gagné !");
+        }
+    }
+}
